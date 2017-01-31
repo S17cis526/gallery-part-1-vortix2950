f1223204278160b3a82ec108f47f67ae6e428d4a
Lunar Eclipse <Lunar Eclipse>
2017-01-31 15:14:49 -0600

    created a simple server

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
-/**
- * server.js
- * This file defines the server for a
- * simple photo gallery web app.
- */
- 
+var http=require('http');
+var port="3325";
+var fs=require('fs');
+
+function userReq (fileName,req,res){
+	fs.readFile("images\\"+fileName,function(err,body){
+		if(err){
+      console.error(err);
+			res.statusCode=500;
+			res.end();
+      return;
+		}
+		res.setHeader("Content-Type","image/jpeg");
+		res.end(body);
+		return;
+});
+
+
+
+}
+
+
+
+
+var server=http.createServer(function(req,res){
+	switch(req.url){
+		case "/chess":
+			userReq("chess.jpg",req,res);
+			break;
+	  default:
+		  res.statusCode = 404;
+	  	res.end("not found");
+	}
+});
+
+
+
+server.listen(port,function(){console.log("listining on port 3325")});
