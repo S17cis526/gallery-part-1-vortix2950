1405a33b99fdc369192ca0887b640e775b0d64bd
Lunar Eclipse <Lunar Eclipse>
2017-02-01 12:09:33 -0600

    just my server

--- server.js
+++ server.js
@@ -1,38 +1,88 @@
-var http=require('http');
-var port="3325";
-var fs=require('fs');
+/**
+ * server.js
+ * This file defines the server for a
+ * simple photo gallery web app.
+ */
+ var http =require ('http');
+ var port= 4566;
+ var fs=require ('fs');
+var stylesheet=fs.readFileSync('gallary.css');
+var imageNames=['ace.jpg','bubble.jpg','chess.jpg','mobile.jpg','fern.jpg'];
 
-function userReq (fileName,req,res){
-	fs.readFile("images\\"+fileName,function(err,body){
+ function  serverImage(filename,req,res){
+
+	fs.readFile('images/'+filename,function(err,body){
 		if(err){
-      console.error(err);
+			console.error(err);
 			res.statusCode=500;
-			res.end();
-      return;
+			res.statusMessage="server error";
+			res.end("silly");
+			return;
 		}
 		res.setHeader("Content-Type","image/jpeg");
 		res.end(body);
-		return;
-});
+	});
+ }
 
 
 
+
+
+ var server = http.createServer(function(req,res){
+
+switch(req.url){
+  case "/gallery":
+var html="<!doctype html>";
+var ghtml=imageNames.map(function(fileName){
+return '<image src='+fileName +' alt="a fishing ace at work">';
+
+
+
+}).join('');
+html+= '<head>';
+html+='<title>Dynamic page></title>'
+html+='<link href ="gallary.css " rel ="stylesheet" type="text/css"> ';
+
+html+='</head>';
+
+html+='<body>';
+html+='<h1>Gallery</h1>';
+html+=gHtml;
+html+='<h1>Hello.</h1> Time is '+Date.now();
+html+='</body>';
+res.setHeader('Content-Type','text/html');
+  res.end(html);
+break;
+case'/gallary.css':
+res.setHeader('Content-Type',"text/css");
+res.end(stylesheet);
+break;
+case'/ace':
+
+
+serverImage('ace.jpg',req,res);
+break;
+  case "/chess":
+	serverImage('chess.jpg',req,res);
+	 break;
+	 case "/fern":
+	 case "/fern/":
+	 case "/fern.jpg":
+	 case "/fern./jpeg":
+	 serverImage('fern.jpg',req,res);
+	 break;
+	 default:
+	res.statusCode=404;
+	res.statusMessage="not Found";
+	res.end();
 }
 
 
+ });
+
+ server.listen(port,function(){
+
+	 console.log("listening on port"+port);
 
 
-var server=http.createServer(function(req,res){
-	switch(req.url){
-		case "/chess":
-			userReq("chess.jpg",req,res);
-			break;
-	  default:
-		  res.statusCode = 404;
-	  	res.end("not found");
-	}
-});
-
-
-
-server.listen(port,function(){console.log("listining on port 3325")});
+ });
