f96334cd85a0c3e8fe683d2dae99e3728921484b
Lunar Eclipse <Lunar Eclipse>
2017-02-05 01:38:07 -0600

    Updated.2/5/17

--- server.js
+++ server.js
@@ -7,88 +7,74 @@ var http =require ('http');
 var port= 4566;
 var fs=require ('fs');
 var stylesheet=fs.readFileSync('gallery.css');
-var imageNames=['mobile.jpg','bubble.jpg','chess.jpg','ace.jpg','fern.jpg'];
- function  serverImage(filename,req,res){
-	fs.readFile('images/'+filename,function(err,body){
-		if(err){
-			console.error(err);
-			res.statusCode=500;
-			res.statusMessage="server error";
-			res.end("silly");
-			return;
-		        }
-		  res.setHeader("Content-Type","image/jpg");
-		  res.end(body);
-	});
- }
-var server = http.createServer(function(req,res){
-switch(req.url){
-    case "/gallery":
-		var html="<!doctype html>";
-		var ghtml=imageNames.map(function(fileName){
-		return '<image src='+fileName +' alt="a fishing ace at work">';
-      }).join('');
-      html+= '<head>';
-      html+='<title>';
-      html+='Dynamic page';
-      html+='</title>';
-      html+='<link href ="gallery.css" rel="stylesheet" type="text/css"> ';
-      html+='</head>';
-      html+='<body>';
-      html+='<h1>Gallery</h1>';
-      html+=ghtml;
-      html+='<h1>Hello.</h1> Time is '+Date.now();
-      html+='</body>';
-      res.setHeader('Content-Type','text/html');
-      res.end(html);
-			break;
-			case'/gallery.css':
-			res.setHeader('Content-Type',"text/css");
-			res.end(stylesheet);
-			break;
-			case "/mobile":
-			case'/mobile':
-			case "/mobile":
-			case "/mobile/":
-			case "/mobile.jpg":
-			case "/mobile./jpeg":
-			serverImage('mobile.jpg',req,res);
-			break;
-			case "/chess":
-			case'/chess':
-			case "/chess":
-			case "/chess/":
-			case "/chess.jpg":
-			case "/chess./jpeg":
-			serverImage('chess.jpg',req,res);
-			break;
-			case "/bubble":
-			case'/bubble':
-			case "/bubble":
-			case "/bubble/":
-			case "/bubble.jpg":
-			case "/bubble./jpeg":
-			serverImage('bubble.jpg',req,res);
-      break;
-      case'/ace':
-      case "/ace":
-      case "/ace/":
-      case "/ace.jpg":
-      case "/ace./jpeg":
-      serverImage('ace.jpg',req,res);
-      break;
-	 		case "/fern":
-		 	case "/fern/":
-		 	case "/fern.jpg":
-		 	case "/fern./jpeg":
-		 	serverImage('fern.jpg',req,res);
-		 	break;
-	 		default:
-			res.statusCode=404;
-			res.statusMessage="not Found";
-			res.end();
+//var imageNames=['mobile.jpg'];
+function getImageNames(callback){
+  fs.readdir('images/',function(err,fileNames){
+    if(err)callback(err,undefined);
+    else callback(false,fileNames);
+  });
 }
+function imageNamesToTags(fileNames){
+  return fileNames.map(function(fileName){
+      return '<img src="'+fileName+'" alt="'+fileName+'">';
+   });
+ }
+function buildGallery(imageTags){
+  var html="<!doctype html>";
+     html+= '<head>';
+     html+='<title>';
+     html+='Dynamic page';
+     html+='</title>';
+     html+='<link href ="gallery.css" rel="stylesheet" type="text/css"> ';
+     html+='</head>';
+     html+='<body>';
+     html+='<h1>Gallery</h1>';
+     html+=imageNamesToTags(imageTags).join('');
+     html+='<h1>Hello.</h1> Time is '+Date.now();
+     html+='</body>';
+      return html;
+    }
+function  serveImage(fileName,req,res){
+  fs.readFile('images/'+fileName,function(err,data){
+    //getImageNames(function(err,imageNames){
+    if(err){
+    	console.error(err);
+    	res.statusCode=500;
+    	res.statusMessage="resoure not found";
+    	res.end("");
+    	return;
+     }
+    res.setHeader("Content-Type","image/jpeg");
+    res.end(data);
+    //	});
  });
-   server.listen(port,function(){
+}
+function serveGallery(req,res){
+  getImageNames(function(err,imageNames){
+      if(err){
+        console.error(err);
+        res.statusMessage='Server error';
+        res.end();
+          return;
+       }
+      res.setHeader('Content-Type','text/html');
+      res.end(buildGallery(imageNames));
+    });
+}
+var server = http.createServer(function(req,res){
+    switch(req.url){
+          case'/':;
+          case "/gallery":
+            serveGallery(req,res);
+            break;
+      		case'/gallery.css':
+      			res.setHeader('Content-Type',"text/css");
+      			res.end(stylesheet);
+      			break;
+       		  default:
+            serveImage(req.url,req,res);
+        }
+ });
+ server.listen(port,function(){
    console.log("listening on port"+port);
  });
